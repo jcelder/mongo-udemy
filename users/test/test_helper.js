389c22e688eb0a67a9f778e233e276e1b85dbce5
Josh <aresx130@gmail.com>
2018-02-25 23:08:31 -0800

    Move mongoose connection to global before hook

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,11 +1,13 @@
+/* eslint-env node, mocha */
 import mongoose from 'mongoose'
 
-const connString = 'mongodb://localhost/users_test'
+before(() => {
+  const connString = 'mongodb://localhost/users_test'
 
-mongoose.connect(connString)
-mongoose.connection
-  .once('open', () => console.log(`Connection successful to ${connString}!`))
-  .on('error', (error) => {
-    console.error('Error:', error)
-  })
-
+  mongoose.connect(connString)
+  mongoose.connection
+    .once('open', () => console.log(`Connection successful to ${connString}!`))
+    .on('error', (error) => {
+      console.error('Error:', error)
+    })
+})
