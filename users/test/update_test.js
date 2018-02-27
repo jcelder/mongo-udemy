bb397934eb772fce8eab129932198775a8e63f26
Josh <aresx130@gmail.com>
2018-02-26 18:24:12 -0800

    Add tests for mongoose update methods

---
+++ users/test/update_test.js
@@ -0,0 +1,62 @@
+/* eslint-env node, mocha */
+
+import { expect } from 'chai'
+import User from '../src/user'
+
+describe('Updating a user record', () => {
+  let josh
+
+  beforeEach(async () => {
+    josh = new User({ name: 'Josh' })
+    await josh.save()
+  })
+
+  // const expectName = async (operation) => {
+  // needs a higher order async wrapper to get mocha error highlighting to work
+  //   try {
+  //     await operation
+  //     const users = await User.find({})
+  //     expect(users.length).to.equal(1)
+  //     expect(users[0].name).to.equal('Jon')
+  //   } catch (e) {
+  //     throw e
+  //   }
+  // }
+
+  it('Model Instance: Set and Save', async () => {
+    // Set and save allows us to possibly update multiple properties on a document and then save
+    josh.set('name', 'Jon')
+    await josh.save()
+    const users = await User.find({})
+    expect(users.length).to.equal(1)
+    expect(users[0].name).to.equal('Jon')
+  })
+
+  it('Model Instance: Update', async () => {
+    await josh.update({ name: 'Jon' })
+    const users = await User.find({})
+    expect(users.length).to.equal(1)
+    expect(users[0].name).to.equal('Jon')
+  })
+
+  it('Model Class: Update', async () => {
+    await User.update({ name: 'Josh' }, { name: 'Jon' })
+    const users = await User.find({})
+    expect(users.length).to.equal(1)
+    expect(users[0].name).to.equal('Jon')
+  })
+
+  it('Model Class: findOneAndUpdate', async () => {
+    await User.findOneAndUpdate({ name: 'Josh' }, { name: 'Jon' })
+    const users = await User.find({})
+    expect(users.length).to.equal(1)
+    expect(users[0].name).to.equal('Jon')
+  })
+
+  it('Model Class: findByIdAndUpdate', async () => {
+    await User.findByIdAndUpdate(josh._id, { name: 'Jon' })
+    const users = await User.find({})
+    expect(users.length).to.equal(1)
+    expect(users[0].name).to.equal('Jon')
+  })
+})
