cf6adb0271aaa0d5bb4b733b3c2a46b1f2e8bd1a
Josh <aresx130@gmail.com>
2018-02-27 05:38:15 -0800

    Add tests for name and postCount field validation

--- users/test/validation_test.js
+++ users/test/validation_test.js
@@ -4,5 +4,24 @@ import { expect } from 'chai'
 import User from '../src/user'
 
 describe('Validating User records', () => {
-
+  it('requires a user name', () => {
+    const user = new User({ name: undefined })
+    const validationResult = user.validateSync()
+    const { message } = validationResult.errors.name
+    expect(message).to.equal('Name is required.')
+  })
+  it('requires a user name to be at least 3 characters long', () => {
+    const user = new User({ name: 'Jo' })
+    const validationResult = user.validateSync()
+    const { message } = validationResult.errors.name
+    expect(message).to.equal('Name must be at least 3 characters long')
+  })
+  it('disallows invalid records from being saved', () => {
+    const user = new User({ name: 'Jo' })
+    return user.save()
+      .catch((validationResult) => {
+        const { message } = validationResult.errors.name
+        expect(message).to.equal('Name must be at least 3 characters long')
+      })
+  })
 })
